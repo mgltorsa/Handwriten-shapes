20dd87fe06a7320988fc32ac58109d543231223b
alejandroMu <alejandrobravo0329@gmail.com>
2019-02-13 20:33:50 -0500

    done calculateLikeeness

--- shape.py
+++ shape.py
@@ -1,6 +1,15 @@
 import copy
+import math
 class Shape:
-    MAXVALUE = 50
+    MAXVALUE = 10
+
+    def __init__(self,matrix):
+        self.__shape=matrix
+        self.__sumValues=0
+        for row in matrix:
+            for column in row:
+                self.__sumValues+=column
+            
 
     def preProcessing(self, matri):
         max = -1
@@ -11,12 +20,39 @@ class Shape:
                     max = column
         increment = (Shape.MAXVALUE-max)/max
         increment += 1
-        #print(increment)
+        
         for row in matrix:
             for  i in range(0,len(row)):
                 row[i] *= increment
                 row[i]=int(row[i])
         return matrix
+    
 
+    def calculateLikeness(self,matrix):
+        matrixSubst=0
+        for row in range(0,len(matrix)):
+            for column in range(len(matrix[0])):
+                matrixSubst+=math.fabs(matrix[row][column]-self.__shape[row][column])
+        tmp= matrixSubst/self.__sumValues
+        return 1-tmp
 
+                
+            
+        
+def test():
+    matrix=[
+        [10,10,10],
+        [1,10,1],
+        [1,10,1]
+    ]
+    matrix1=[
+        [9,3,8],
+        [5,6,1],
+        [1,6,2]
+    ]
+    shape=Shape(matrix)
+    var=shape.preProcessing(matrix1)
+    result=shape.calculateLikeness(var)
+    print(result)
 
+test()
